f0aa6bbbc5a52b357057bf1e626d57e9b39cf8bb
edpollitt <edpollitt@gmail.com>
2015-07-24 17:28:41 +0100

    Fixed value mapping tests

---
+++ Nerdle.AutoConfig.Tests.Unit/Mapping/MappingFromFixedValueTests/When_mapping_a_property.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Nerdle.AutoConfig.Mapping;
+using NUnit.Framework;
+
+namespace Nerdle.AutoConfig.Tests.Unit.Mapping.MappingFromFixedValueTests
+{
+    [TestFixture]
+    public class When_mapping_a_property
+    {
+        [TestCase(1)]
+        [TestCase("hello")]
+        [TestCase('x')]
+        [TestCase(DayOfWeek.Monday)]
+        public void The_value_is_applied(object value)
+        {
+            var propertyMapping = new MappingFromFixedValue(typeof(Foo).GetProperty("Bar"), value);
+            var instance = new Foo();
+            propertyMapping.Apply(instance);
+            instance.Bar.Should().Be(value);
+        }
+
+        class Foo
+        {
+            public object Bar { get; set; }
+        }
+     }
+}
