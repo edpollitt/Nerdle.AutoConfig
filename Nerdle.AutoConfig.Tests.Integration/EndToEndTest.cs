a544c436f4d713b159174f1c27543587502b6232
Cédric Luthi <cedric.luthi@gmail.com>
2018-09-21 13:37:19 +0200

    Upgrade NUnit to version 3.10.1
    This enables running tests on .NET Core

--- Nerdle.AutoConfig.Tests.Integration/EndToEndTest.cs
+++ Nerdle.AutoConfig.Tests.Integration/EndToEndTest.cs
@@ -5,6 +5,12 @@ namespace Nerdle.AutoConfig.Tests.Integration
     [TestFixture]
     public abstract class EndToEndTest
     {
+        public EndToEndTest()
+        {
+            // See https://github.com/nunit/nunit/issues/1072#issuecomment-218725978
+            System.Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+        }
+        
         public string ConfigFilePath => GetType().Name + ".xml";
     }
 }
