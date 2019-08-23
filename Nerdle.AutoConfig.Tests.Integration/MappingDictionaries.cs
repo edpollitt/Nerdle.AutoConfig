105a02a8cd599092a8a7557d9eb6166143b919d7
Cédric Luthi <cedric.luthi@otis.com>
2019-08-23 12:05:19 +0200

    Add support for mapping IReadOnlyDictionary

--- Nerdle.AutoConfig.Tests.Integration/MappingDictionaries.cs
+++ Nerdle.AutoConfig.Tests.Integration/MappingDictionaries.cs
@@ -19,9 +19,31 @@ namespace Nerdle.AutoConfig.Tests.Integration
         }
 
         [Test]
+        public void Mapping_readOnlyDictionaries()
+        {
+            var config = AutoConfig.Map<IReadOnlyDictionaryConfiguration>(configFilePath: ConfigFilePath);
+            config.Should().NotBeNull();
+            config.FrenchWords.Should().HaveCount(3);
+            config.FrenchWords.Keys.Should().ContainInOrder("cat", "dog", "monkey");
+            config.FrenchWords.Values.Should().ContainInOrder("chat", "chien", "singe");
+        }
+
+        [Test]
         public void Mapping_complex_dictionaries()
         {
             var config = AutoConfig.Map<IComplexDictionaryConfiguration>(configFilePath: ConfigFilePath);
+            config.Should().NotBeNull();
+            config.Endpoints.Should().HaveCount(2);
+            config.Endpoints["Primary"].Should().HaveCount(2);
+            config.Endpoints["Primary"].First().Name.Should().Be("Foo");
+            config.Endpoints["Primary"].First().Port.Should().Be(1);
+            config.Endpoints["Secondary"].Should().HaveCount(1);
+        }
+
+        [Test]
+        public void Mapping_complex_readOnlyDictionaries()
+        {
+            var config = AutoConfig.Map<IComplexReadOnlyDictionaryConfiguration>(configFilePath: ConfigFilePath);
             config.Should().NotBeNull();
             config.Endpoints.Should().HaveCount(2);
             config.Endpoints["Primary"].Should().HaveCount(2);
@@ -35,9 +57,19 @@ namespace Nerdle.AutoConfig.Tests.Integration
             IDictionary<string, string> FrenchWords { get; }
         }
 
+        public interface IReadOnlyDictionaryConfiguration
+        {
+            IReadOnlyDictionary<string, string> FrenchWords { get; }
+        }
+
         public interface IComplexDictionaryConfiguration
         {
             IDictionary<string, IEnumerable<IEndpointConfiguration>> Endpoints { get; }
+        }
+
+        public interface IComplexReadOnlyDictionaryConfiguration
+        {
+            IReadOnlyDictionary<string, IEnumerable<IEndpointConfiguration>> Endpoints { get; }
         }
 
         public interface IEndpointConfiguration
